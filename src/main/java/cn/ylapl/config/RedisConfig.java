58e99d3c07aa45ab20ea6d2383c24eb74774b93d
杨乐 <yangle@xueleyun.com>
2017-03-20 21:38:56 +0800

    1.redisTemp补全

--- src/main/java/cn/ylapl/config/RedisConfig.java
+++ src/main/java/cn/ylapl/config/RedisConfig.java
@@ -62,7 +62,7 @@ public class RedisConfig extends CachingConfigurerSupport {
 
         RedisTemplate<String,Object> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory);
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
