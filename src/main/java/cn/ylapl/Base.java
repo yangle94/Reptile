291ebdb5920d290110dfb5388c6cf8459edfd6b0
杨乐 <yangle@ewell.cc>
2018-04-28 16:30:49 +0800

    清理文件

--- src/main/java/cn/ylapl/Base.java
+++ src/main/java/cn/ylapl/Base.java
@@ -4,20 +4,14 @@
  */
 package cn.ylapl;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author yangle
  * @version $Id Base.java, v 0.1 2017-01-21 下午5:18 yangle Exp $$
  */
 @SpringBootApplication
-@EnableScheduling
-@EnableCaching
-@MapperScan(value = "cn.ylapl.mapper")
 public class Base {
 
     public static void main(String arg[]) {
